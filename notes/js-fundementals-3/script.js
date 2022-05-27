df584827fec05a8f4e87ccad2599b00947933e0d
Taylor Casse <taylorcasse@gmail.com>
2022-05-27 08:48:26 +0200

    Added the beginnings of a function to return the third highest value in an array

--- notes/js-fundementals-3/script.js
+++ notes/js-fundementals-3/script.js
@@ -1,102 +1,112 @@
 // Functions
+// Hoisting
+function hoistingDemo() {
+    // Note: functions are always hoisted
+    myFunction();
 
-
-// Note: functions are always hoisted
-myFunction();
-
-// Standard function declaration
-function myFunction() {
-    console.log('Hello, World!');
-}
-myFunction();
-
-// Anonymous functions are unnamed and thus can't be reused
-// They must be placed in brackets  
-(function () {
-    console.log('This is an anonymous function');
-})
-
-// Unless they're assigned to a vairable
-const anonFunction = function () {
-    console.log('Anonymous function assigned to a variable');
-}
-anonFunction();
-
-// Arrow function
-// Often used for listener events
-const button = document.querySelector('button');
-const para = document.querySelector('p');
-button.addEventListener('click', () => {
-    console.log('Working');
-    // Use .value to get text from an input field
-    // const input = document.getElementById('user-input').value;
-    const input = document.querySelector('#user-input').value;
-    para.textContent = input;
-})
-
-// Function with default parameter
-// Note: parameters are variables that you can pass into a function
-// arguments are the values of those parameters
-// The function below has a default value for param
-function myFunction2(param='This is an argument') {
-    // return is what the called function evaluates to
-    // It also stops the function and breaks out of its scope
-    return param;
-    console.log('This will not run');    // Will not run because it follows a return statement
-}
-console.log(myFunction2()); // 'This is the default'
-sampleString = 'This is a user speficied argument'
-console.log(myFunction2(sampleString));   // 'This is a user speficied argument'
-
-
-// isNaN vs Number.isNaN
-// Still don't fully understand this
-
-const testArr = [NaN, 'A string', 10, undefined, true, [], 'A string'/2];
-
-// isNaN is useful for checking inputs
-// confusing because it checks if something isn't a number, rather than is NaN
-const newArr2 = testArr.map((value) => isNaN(value));
-console.log(newArr2);   // [true, true, false, true, false, false, true]
-
-// Number.isNaN
-// Fixes the problems with isNaN, returns true only if parameter === NaN (?)
-const newArr = testArr.map((value) => Number.isNaN(value));
-console.log(newArr);    // [true, false, false, false, false, false]
-
-// But NaN === NaN returns false 
-const newArr3 = testArr.map((value) => value === NaN);
-console.log(newArr3);   // [false, false, false, false, false, false]
-
-
-// Trying out functions as parameters
-function test1(value) {
-    // return arr.map( (value) => isNaN(value) );
-    return isNaN(value)
-}
-
-function test2(arr) {
-    return arr.map((value) => Number.isNaN(value));
-}
-
-function testingNaN(arr, func) {
-    for (let i = 0; i < arr.length; i++ ) {
-        console.log(arr[i] + ': ' + func(arr[i]));
+    // Standard function declaration
+    function myFunction() {
+        console.log('Hoisting demo');
     }
-}    
+    myFunction();
+}
+// hoistingDemo();
 
-testingNaN(testArr, test1);
+function anonFunctionDemo() {
+    // Anonymous functions are unnamed and thus can't be reused
+    // They must be placed in brackets  
+    (function () {
+        console.log('This is an anonymous function');
+    })
 
-// I now understand this. isNaN() tries to coerce its arguments to numbers,
-// thus arguments with a primitive numeric value (booleans, empty strings/arrays) will
-// be coerced into their numeric value. Number.isNaN() doesn't perform this coersion
-console.log(+[]);   // 0
-console.log(+'');   // 0
-console.log(+true); // 1
+    // Unless they're assigned to a vairable
+    const anonFunction = function () {
+        console.log('This is an anonymous function assigned to a variable');
+    }
+    anonFunction();
 
-// sidenote: isNaN exists purely because NaN can't be compaired to anything, even itself
-// it is the only datatype that does this
-console.log(NaN == NaN);    // false
+    // Arrow function
+    // Often used for listener events
+    const button = document.querySelector('button');
+    const para = document.querySelector('p');
+    button.addEventListener('click', () => {
+        console.log('Working');
+        // Use .value to get text from an input field
+        // const input = document.getElementById('user-input').value;
+        const input = document.querySelector('#user-input').value;
+        para.textContent = input;
+    })
+}
+// anonFunctionDemo();
+
+function defaultParameterDemo() {
+    // Function with default parameter
+    // Note: parameters are variables that you can pass into a function
+    // arguments are the values of those parameters
+    // The function below has a default value for param
+    function myFunction2(param='This is the default argument') {
+        // return is what the called function evaluates to
+        // It also stops the function and breaks out of its scope
+        return param;
+        console.log('This will not run');    // Will not run because it follows a return statement
+    }
+    console.log(myFunction2()); // 'This is the default'
+    sampleString = 'This is a user speficied argument'
+    console.log(myFunction2(sampleString));   // 'This is a user speficied argument'
+}
+// defaultParameterDemo();
+
+function isNaNDemo() {
+    // isNaN vs Number.isNaN
+    // Still don't fully understand this
+
+    const testArr = [NaN, 'A string', 10, undefined, true, [], 'A string'/2];
+
+    // isNaN is useful for checking inputs
+    // confusing because it checks if something isn't a number, rather than is NaN
+    const newArr2 = testArr.map((value) => isNaN(value));
+    console.log(newArr2);   // [true, true, false, true, false, false, true]
+
+    // Number.isNaN
+    // Fixes the problems with isNaN, returns true only if parameter === NaN (?)
+    const newArr = testArr.map((value) => Number.isNaN(value));
+    console.log(newArr);    // [true, false, false, false, false, false]
+
+    // But NaN === NaN returns false 
+    const newArr3 = testArr.map((value) => value === NaN);
+    console.log(newArr3);   // [false, false, false, false, false, false]
+
+
+    // Trying out functions as parameters
+    function test1(value) {
+        // return arr.map( (value) => isNaN(value) );
+        return isNaN(value)
+    }
+
+    function test2(arr) {
+        return arr.map((value) => Number.isNaN(value));
+    }
+
+    function testingNaN(arr, func) {
+        for (let i = 0; i < arr.length; i++ ) {
+            console.log(arr[i] + ': ' + func(arr[i]));
+        }
+    }    
+
+    testingNaN(testArr, test1);
+
+    // I now understand this. isNaN() tries to coerce its arguments to numbers,
+    // thus arguments with a primitive numeric value (booleans, empty strings/arrays) will
+    // be coerced into their numeric value. Number.isNaN() doesn't perform this coersion
+    console.log(+[]);   // 0
+    console.log(+'');   // 0
+    console.log(+true); // 1
+
+    // sidenote: isNaN exists purely because NaN can't be compaired to anything, even itself
+    // it is the only datatype that does this
+    console.log(NaN == NaN);    // false
+}
+// isNaNDemo();
 
 
 // Leap year calculator for fun and to prove a point
@@ -115,12 +125,6 @@ function isLeapYear(year) {
         return 'Not a leap year.';
     }
 }
-
-// const startYear = 1970;
-// for ( let i = 0; i < 100; i++) {
-//     currentYear = startYear + i;
-//     console.log(currentYear + ': ' + isLeapYear(currentYear));
-// }
 
 document.getElementById('fizz-button').addEventListener('click', () =>{
     const inputNumber = document.getElementById('fizz-input').value;
@@ -177,6 +181,7 @@ function primes(startNum, endNum) {
 
 
 function primeCheck(num) {
+    // Checks if num is prime by checking the mod of num and all numbers smaller than num
     let isPrime = true;
     for (let i = 0; i < num; i++) {
         if (num % i === 0 && ![0,1].includes(i)) {
@@ -187,6 +192,7 @@ function primeCheck(num) {
 }
 
 function primeGen(num) {
+    // Returns all prime numbers from 0 to num
     const primeArr = [];
     for (let i = 0; i <= num; i++) {
         if (primeCheck(i)) {
@@ -196,12 +202,63 @@ function primeGen(num) {
     return primeArr.join(' ');
 }
 
-console.log(primeGen(100));
+// console.log(primeGen(100));
 
-// return the third highest number in an array
-// Psudocode: 
-// input an array of numbers of any size
-// find the largest number, pop it off x3
-// return the third number popped from the array
+// Array.reduce()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+// A callback function is a function passed as an argument to another function
+// .reduce() applies a user-defined callback functuon on each element in an array, in order,
+// passing in the return value from the calculation on the previous value.
 
-const arr = [1, 2, 3, 4, 5, 6];
+
+const reduceArr = [1, 2, 3, 4, 5];
+function reduceDemo(arr) {
+    return arr.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+}
+// console.log(`Array: ${reduceArr} \nSum: ${reduceDemo(reduceArr)}`)
+
+function thirdHighestDemo() {
+    // Third Highest - return the third highest number in an array
+    // Psudocode: 
+    // input an array of numbers of any size
+    // find the largest number, pop it off x3
+    // return the third number popped from the array
+
+    const thirdHighestArr = [1, 2, 3, 4, 5, 6];
+
+    // Using a loop
+    console.log('Third highest using loop');
+    function thirdHighestLoop(arr) {
+        let highestValue = 0;
+        let highestValueIndex = 0;
+        for (let j = 0; j < 3; j++) {
+            for (let i = 0; i < arr.length; i++) {
+                console.log('outerloop');
+                if (highestValue < arr[i]) {
+                    highestValue = arr[i];
+                    highestValueIndex = i;
+                    console.log(`Highest value: ${highestValue}\nIteration: ${i}`);
+                }
+            }
+            // .splice() edits an array by adding or removing elements at a given index
+            console.log(arr.splice(highestValueIndex, 1));
+            arr.splice(highestValueIndex, 1);
+            // let someVar = j;
+        }
+        return arr;
+    }
+
+    console.log(`Third highestoutput: ${thirdHighestLoop(thirdHighestArr)}`);
+
+    // Using the array.reduce() method
+    function getMaxOfArray(numArr) {
+        return Math.max.apply(null, numArr);
+    }
+
+    function thirdHighest(arr) {
+        return getMaxOfArray(arr);
+    }
+
+    console.log(thirdHighest());
+}
+thirdHighestDemo();
