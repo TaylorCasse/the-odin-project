59bd4d342375de9faabf8870192c236adcf131d6
Taylor Casse <taylorcasse@gmail.com>
2022-06-08 08:46:36 +0200

    Completed exercise 8

--- js-fundementals-4/exercises/javascript-exercises/08_calculator/calculator.js
+++ js-fundementals-4/exercises/javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(...args) {
+  let result = 0;
+  for (let num of args) {
+    result += num;
+  }
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(startNum, ...args) {
+	let result = startNum;
+  for (let num of args) {
+    result -= num;
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  // Checks if the input is in the form of an array or multiple arguments and handles each case
+  if (args.length === 1) {
+    args = [...args[0]];
+  }
+
+	return +args.reduce((previous, current) => previous + current, 0);
 };
 
-const multiply = function() {
+console.log(sum(1, 2, 3));
 
+const multiply = function(...args) {
+  if (args.length === 1) {
+    args = [...args[0]];
+  }
+
+	return +args.reduce((previous, current) => previous*current, 1);
 };
 
-const power = function() {
-	
+const power = function(...args) {
+  if (args.length === 1) {
+    args = [...args[0]];
+  }
+	return +args.reduce((previous, current) => previous**current);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    const factorialArray = [...Array(num).keys()].map(item => item + 1);
+    return multiply(factorialArray);
+  }
 };
 
 // Do not edit below this line
