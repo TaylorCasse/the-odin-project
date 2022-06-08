1d27aadd86d867cb325aa93ab9880ac77f9e20f5
Taylor Casse <taylorcasse@gmail.com>
2022-06-08 07:29:04 +0200

    Completed exercise 7

--- js-fundementals-4/exercises/javascript-exercises/07_tempConversion/tempConversion.js
+++ js-fundementals-4/exercises/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function(tempInF) {
+  const tempInC = (tempInF - 32) * (5 / 9);
+  return Math.round(tempInC * 10) / 10;
 
 };
 
-const ctof = function() {
-
+const ctof = function(tempInC) {
+  const tempInF = (tempInC * (9 / 5) + 32);
+  return Math.round(tempInF * 10) / 10;
 };
 
 // Do not edit below this line
