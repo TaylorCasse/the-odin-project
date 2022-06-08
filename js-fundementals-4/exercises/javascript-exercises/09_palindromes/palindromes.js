9f762fc1c2d8cbac34dd7b1c47eaf572fc00f6ef
Taylor Casse <taylorcasse@gmail.com>
2022-06-08 09:10:04 +0200

    Nearly finished exercise 9

--- js-fundementals-4/exercises/javascript-exercises/09_palindromes/palindromes.js
+++ js-fundementals-4/exercises/javascript-exercises/09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    const lettersOnly = inputString.replace(/[/\s.,\/#!$%\^&\*;:{}=\-_`~()@]/g,"").toLowerCase();
+    if (lettersOnly === [...lettersOnly].reverse().join('')) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
+const testArr = ['A car, a man, a maraca.', 'Rats live on no evil star.', 'Lid off a daffodil.', 'Animal loots foliated detail of stool lamina.', 'A nut for a jar of tuna', 'A ! string!@#$']; 
+const newArr = testArr.map(palindromes);
+console.log(newArr);
+console.log(palindromes('A ! string!@#$'));
 // Do not edit below this line
 module.exports = palindromes;
